7ecd0770b6666afac89ebbfc6ee580ede20a19de
Soyeb <tssoyeb@gmail.com>
2022-05-23 17:59:35 +0600

    fetch data from backend

--- src/components/Home/Products.js
+++ src/components/Home/Products.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Product from './Product';
 
 const Products = () => {
+    const [products,setProducts]=useState([])
+    useEffect(()=>{
+        fetch('http://localhost:5000/products')
+        .then(res=>res.json())
+        .then(data=>{console.log(data);})
+    },[])
     return (
         <div>
             <p>Products part</p>
+            <Product></Product>
         </div>
     );
 };
