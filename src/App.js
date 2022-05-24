c11b237ecc40031cda209b3aac35a456052c0002
Soyeb <tssoyeb@gmail.com>
2022-05-24 11:03:29 +0600

    sign up established

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Blogs from './components/Blogs';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
+import SignUp from './components/Login/SignUp';
 import Purchase from './components/Purchase';
 import Navbar from './components/Shared/Navbar';
 
@@ -17,6 +18,7 @@ function App() {
         <Route path='/home/:id' element={<Purchase></Purchase>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='signup' element={<SignUp></SignUp>}></Route>
       </Routes>
     </div>
   );
