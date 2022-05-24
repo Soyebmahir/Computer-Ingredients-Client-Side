fdd050b629ae7f88d51fec69f8b2ace18e059e24
Soyeb <tssoyeb@gmail.com>
2022-05-24 10:27:06 +0600

    login established

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Blogs from './components/Blogs';
 import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 import Purchase from './components/Purchase';
 import Navbar from './components/Shared/Navbar';
 
@@ -15,6 +16,7 @@ function App() {
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/home/:id' element={<Purchase></Purchase>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
       </Routes>
     </div>
   );
